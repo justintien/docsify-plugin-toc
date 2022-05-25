8a93c691f3b8ab2af93fdb16981525d29fbcaf78
JustinTien <thitbbeb+git-commit@gmail.com>
2022-05-26 00:32:25 +0800

    build: bump all deps & js/css standalone

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,55 @@
 const path = require('path')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const pluginName = 'docsify-plugin-toc'
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: `${pluginName}.min.js`,
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    [pluginName]: [path.join(process.cwd(), 'src', 'index.js')],
+    [pluginName + '.min']: [path.join(process.cwd(), 'src', 'index.js')]
   },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
+  output: {
+    path: path.join(process.cwd(), 'dist'),
+    filename: '[name].js',
+    library: 'DocsifyPluginToc',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    sourceMapFilename: '[file].map',
+    globalObject: 'this'
+  },
+  optimization: {
+    minimizer: [
+      new TerserWebpackPlugin({
+        include: /\.min\.js$/,
+        parallel: true,
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false
+          },
+          compress: true,
+          ie8: false,
+          ecma: 5,
+          warnings: false
+        }
+      })
     ]
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.join(process.cwd(), 'src', 'assets', 'light.css'),
+          to: path.join(process.cwd(), 'dist', 'light.css')
+        },
+        {
+          from: path.join(process.cwd(), 'src', 'assets', 'dark.css'),
+          to: path.join(process.cwd(), 'dist', 'dark.css')
+        }
+      ]
+    })
+  ],
+  module: {}
 }
